415558d2d655c2654dd48bbd40e25fa2b1f0ff50
Sanjay KP <sanjutechzzz@gmail.com>
2026-07-06 20:03:14 +0530

    Configure GitHub Pages deployment settings

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/to-do-list/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
